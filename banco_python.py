6b2c4693e66690f3436ce790b2bb6643e3b0defd
flaviodossantos <flaviovorthrox@yahoo.com.br>
2025-06-20 21:39:32 -0300

    :sparkles: feat: add deposito e finalizando o programa

--- banco_python.py
+++ banco_python.py
@@ -12,7 +12,7 @@ Digite um numero correspondente a operação que deseja realizar :
 """
 saldo = 0
 extrato = ""
-num_saques = 0
+numero_saques = 0
 LIMITE_POR_SAQUE = 500
 LIMITE_SAQUES_DIARIO = 3
 
@@ -28,9 +28,28 @@ while True:
             print("\n=====>>>> Digite um valor válido. <<<<======")
 
     elif operacao == '2':
+        valor =  float(input("Digite o valor do Saque : "))
+        
+        excedeu_saldo = valor > saldo
+        excedeu_limite = valor > LIMITE_POR_SAQUE
+        excedeu_saques = numero_saques >= LIMITE_SAQUES_DIARIO
+
+        if excedeu_saldo:
+            print("Você não tem saldo suficiente.")
+
+        elif excedeu_limite:
+            print("O valor do saque excede o limite por saque.")
+
+        elif excedeu_saques:
+            print("Número máximo de saques diários excedido.")
+
+        elif valor > 0:
+            saldo -= valor
+            extrato += f"Saque: R$ {valor:.2f}\n"
+            numero_saques += 1
 
     elif operacao == '3':
-        print("\n-------------- Extrato --------------")  
+        print("\n-------------- Extrato --------------\n")  
         if extrato == '':
             print("\n=====>>>> Não foram realizadas operações. <<<<======")
         else :
@@ -41,3 +60,7 @@ while True:
     elif operacao == '0':
         print("********* Obrigado por usar o Banco Python. ***********\n")
         break
+
+    else:
+        print("\n=====>>>> Digite um numero válido para operação desejada. <<<<======")
+
